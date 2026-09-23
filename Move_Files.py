e40ac637eb4a17569008cddee2f3ecec19ed70ae
Tomi <tbanjoko25@gmail.com>
2026-09-23 13:15:05 -0400

    Fix string formatting for AppName in Move_Files.py and ensure proper directory handling

--- Move_Files.py
+++ Move_Files.py
@@ -2,18 +2,19 @@ import subprocess
 from tkinter import filedialog, messagebox
 from os.path import dirname,abspath
 import os
-
+AppName=''
+# set direct variable to True to make it go directly to a Local AppData folder
+direct=False
 def get_destination(direct:bool =False)-> str:
     if direct:
-        title= "Select {AppName} folder... "
+        title= f"Select {AppName} folder... "
         folder_path = filedialog.askdirectory(title=title)
     else:
-        folder_path = os.path.join(os.environ["LOCALAPPDATA"], "AppName")
+        folder_path = os.path.join(os.environ["LOCALAPPDATA"], f"{AppName}")
     return folder_path
 def get_path()-> str:
     path=dirname(abspath(__file__))+ "\\assets"
     return  path 
-
-cmd = ["robocopy", get_path(),get_destination(), '/s']
+cmd = ["robocopy", get_path(),get_destination(direct), '/s']
 subprocess.run(cmd, stdout=subprocess.DEVNULL)
-messagebox.showwarning("Restart {AppName}","You will need to restart {AppName} by saving and closing. Only AFTER will the additional features be implemented.")
+messagebox.showwarning(f"Restart {AppName}","You will need to restart {AppName} by saving and closing. Only AFTER will the additional features be implemented.")
